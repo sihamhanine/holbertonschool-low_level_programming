f488e2006ca9ba751e336b65824c41fae79ff736
sihamhanine <siham.elani17@gmail.com>
2024-02-20 09:02:48 +0000

    (Task 0. This program will assign a random number to the variable n each time it is executed. Complete the source code in order

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
@@ -18,9 +17,9 @@ printf("%d is zero\n", n);
 else
 if (n < 0)
 printf("%d is negative\n", n);
-	
 return (0);
 }
+
 
 
 
