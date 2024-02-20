75fa638bde841dc15effdb074fdeca31c77a1627
sihamelani <siham.elani17@gmail.com>
2024-02-20 18:15:59 +0000

    Task 1.

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 int n, last;
-srand (time(0));
+srand(time(0));
 n = rand() - RAND_MAX / 2;
 last = n % 10;
 
@@ -32,6 +32,7 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 
 return (0);
 }
+
 
 
 
