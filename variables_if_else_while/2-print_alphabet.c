a77f4d239570e1c983c0093c0ef431ad79e05f39
sihamelani <siham.elani17@gmail.com>
2024-02-20 19:23:55 +0000

    Task.2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+  char c;
+  for(c = 'a'; c <= 'z' ; c++)
+    {
+      putchar(c);
+    }
+  putchar('\n');
+  return (0);
+}
+
