d88b061d6a4efd13e701ed033fec446654a894b5
sihamelani <siham.elani17@gmail.com>
2024-02-21 16:24:47 +0000

    Task 9. Write a program that prints all possible combinations of single-digit numbers.

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
  * main - Paint "Write a program that prints all possible
  * combinations of single-digit numbers.
  *
- * Return: Always 0 NOT NULL.
+ * Return: 0 NOT NULL.
  */
 int main(void)
 {
@@ -20,16 +20,3 @@ putchar(' ');
 putchar('\n');
 return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-	
