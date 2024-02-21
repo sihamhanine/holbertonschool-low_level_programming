59673f6217c0db65859c94d328b29a4d9b1acb6b
sihamelani <siham.elani17@gmail.com>
2024-02-21 10:24:40 +0000

    Task 4. Write a program that prints the alphabet in lowercase, followed by a new line.

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 /**
- * main -Paint "Write a program that prints the alphabet in lowercase, followed by a new line."
- *
+ * main -Paint "Write a program that prints the alphabet 
+ * in lowercase, followed by a new line."
  *
  * Return: 0 NOT NULL.
  */
 int main(void)
 {
   char c;
-
   for (c = 'a'; c <= 'z'; c++)
   {
-  if (c != 'q' && c != 'e')
-  {  
+  if (c != 'q' && c != 'e)
+  {
   putchar(c);
   }
   }
   putchar('\n');
   return (0);
 }
+
 
