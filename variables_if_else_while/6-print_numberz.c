b21ffe7490f4400b7ae87b3dcac42747ec55cd07
sihamelani <siham.elani17@gmail.com>
2024-02-21 13:42:19 +0000

    Task6. Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Paint "Write a program that prints all single digit numbers 
+ * of base 10 starting from 0, followed by a new line.
+ *
+ *
+ * Return: 0 NOT NULL.
+ */
+int main(void)
+{
+  int n;
+  for (n = '0' ; n <= '9' ; n++)
+    {
+      putchar(n);
+    }
+  putchar('\n');
+  return (0);
+}
+
+
