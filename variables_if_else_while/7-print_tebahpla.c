840f3ce4618b196e4d6db4097db8503e027c19cd
sihamelani <siham.elani17@gmail.com>
2024-02-21 14:16:42 +0000

    Task7 .Write a program that prints the lowercase alphabet in reverse, followed by a new line.

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
  * main - Paint "Write a program that prints the lowercase
- *  alphabet in reverse, followed by a new line
+ * alphabet in reverse, followed by a new line
  *
- * 
+ *
  * Return:0 NOT NULL.
  */
 int main(void)
 {
-  char n;
-
-  for (n = 'z' ; n >= 'a' ; n--)
-    {
-      putchar(n);
-    }
-  putchar('\n');
-  return (0);
+char n;
+for (n = 'z' ; n >= 'a' ; n--)
+{
+putchar(n);
 }
+putchar('\n');
+return (0);
+}
+
 
