70516e18649287dcb8480041db720cdd1739da34
sihamelani <siham.elani17@gmail.com>
2024-02-22 14:08:34 +0000

    Task1. Write a function that prints the alphabet, in lowercase, followed by a new line.

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,19 +4,14 @@
  * Function that prints the alphabet, in lowercase, followed by a new line
  *
  *
- * Return: 0 NOT NULL.
  */
 
 void print_alphabet(void)
 {
-  char c;
-
-  for (c = 'a' ; c <= 'z' ; c++)
-    {
-      _putchar(c);
-    }
-  _putchar('\n');
-    
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
 }
-
-
+_putchar('\n');
+}
