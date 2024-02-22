384d8d1aecd9722407f71e47cad8c926ff113da6
sihamelani <siham.elani17@gmail.com>
2024-02-22 13:41:29 +0000

    Task0 Write a program that prints _putchar, followed by a new line.

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Print "Write a program that prints _putchar, followed by a new line."
+ *
+ *
+ * Return: 0 NOT NULL.
+ */
+int main(void)
+{
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+
+  return (0);
+
+}
+
+
