4f7918a385d81c8fc7183c4abd285fe54331a12b
sihamelani <siham.elani17@gmail.com>
2024-02-23 10:08:47 +0000

    Task7 print the last digit

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Function that prints every minute of 
+ * the day of Jack Bauer, starting 
+ * from 00:00 to 23:59.
+ *
+ */
+
+void jack_bauer(void)
+{
+  int h, m;
+
+  for (h = 0 ;
