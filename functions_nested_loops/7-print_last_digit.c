d8ace63aa60055c956dbf31bea00cec42ebf3427
sihamelani <siham.elani17@gmail.com>
2024-02-23 10:29:35 +0000

    Task7 print the last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,25 +10,17 @@
 */
 int print_last_digit(int n)
 {
-  int last;
-  last = n % 10;
-  if (last > 0)
-    {
-      _putchar(last + '0');
-      return (last);
-    }
-  else
-    {
-   
-      _putchar(-last + '0');
-      return (-last);
-    }
- 
+int last;
+last = n % 10;
+if (last > 0)
+{
+_putchar(last + '0');
+return (last);
 }
-
-
-
-
-
-
+else
+{
+_putchar(-last + '0');
+return (-last);
+}
+}
 
