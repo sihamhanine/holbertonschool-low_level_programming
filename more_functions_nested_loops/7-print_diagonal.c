04c9dd7eed5d67fdb38ef24dd347e2bb8c6e2bb6
sihamelani <siham.elani17@gmail.com>
2024-02-26 14:07:00 +0000

    Task 7.Write a function that draws a diagonal line on the terminal.

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@
  */
 void print_diagonal(int n)
 {
-int i , j;
+int i, j;
 if (n <= 0)
 {
 _putchar('\n');
@@ -18,10 +18,10 @@ _putchar('\n');
 else
 for (i = 0 ; i < n ; i++)
 {
-for (j = 0 ; j < i ; j++)   
+for (j = 0 ; j < i ; j++)
 {
 _putchar(' ');
- }
+}
 _putchar('\\');
 _putchar('\n');
 
