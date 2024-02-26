f603233aca2fb752a01dbb0f53a809e4f2ea6845
sihamelani <siham.elani17@gmail.com>
2024-02-26 08:47:02 +0000

    Task2. Write a function that multiplies two integers.

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - Function that multiplies two integers.
+ *
+ *
+ * @a: le nombre a verifier
+ * @b: le nombre a verifier
+ *
+ * Return: la multiplication des deux nombres.
+ */
+
+int mul(int a, int b)
+{
+int mult;
+
+mult = a * b;
+
+return (mult);
+}
