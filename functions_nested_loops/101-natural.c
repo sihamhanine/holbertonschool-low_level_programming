ad5470f20a7afb187d4fca37ca56cfdaf0bab732
sihamelani <siham.elani17@gmail.com>
2024-03-02 14:40:14 +0000

    Task 13

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -9,16 +9,15 @@
 
 int main(void)
 {
-  int i , z = 0;
-
-  while (i < 1024)
-    {
-      if ((i % 3 == 0) || (i % 5 == 0))
-	{
-	  z = z + i;
-	}
-      i++;
-    }
-  printf("%d\n", z);
-  return (0);
+int i , z = 0;
+while (i < 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+z = z + i;
+}
+i++;
+}
+printf("%d\n", z);
+return (0);
 }
