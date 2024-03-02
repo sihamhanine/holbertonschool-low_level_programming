77f3efff36b98a515b9cac2b126b30c21570b2be
sihamelani <siham.elani17@gmail.com>
2024-03-02 20:49:11 +0000

    Task 9

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  prints exactly and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19
+ *
+ *
+ * Return: 1 NOT NULL
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19, \n");
+return (1);
+}
