3e673e53e13f6aa159683b8c8469ceb0a556cea2
sihamelani <siham.elani17@gmail.com>
2024-03-02 15:09:56 +0000

    Task 14

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -14,25 +14,24 @@ int main(void)
   long j = 1, k = 2;
 
   while (i < 50)
-    {
-      if (i == 0)
-	{
-	  printf("%ld", j);
-	}
-      else
-	if (i == 1)
-	  {
-	    printf(", %ld", k);
-	  }
-	else
-	  {
-	    k = k + j;
-	    j = k - j;
-	    printf(", %ld", k);
-	  }
-      ++i;
-    }
+  {
+  if (i == 0)
+  {
+  printf("%ld", j);
+  }
+  else
+  if (i == 1)
+  {
+  printf(", %ld", k);
+  }
+  else
+  {
+  k = k + j;
+  j = k - j;
+  printf(", %ld", k);
+  }
+  ++i;
+  }
   printf("\n");
   return (0);
 }
-
