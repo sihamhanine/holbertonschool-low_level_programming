e9bc3ea09ae8499d599e11dc3bb2f4fcfa3e58d0
sihamelani <siham.elani17@gmail.com>
2024-03-02 15:23:59 +0000

    Task 15

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -9,18 +9,16 @@
 
 int main(void)
 {
-  int i = 0;
-  long j = 1, k = 2, sum = k;
-  while (k + j < 4000000)
-    {
-      k = k + j;
-      if (k % 2 == 0)
-	sum = sum + k;
-      j = k - j;
-    
-  
-  ++i;
-    }
-  printf("%ld", sum);
-  return (0);
+int i = 0;
+long j = 1, k = 2, sum = k;
+while (k + j < 4000000)
+{
+k = k + j;
+if (k % 2 == 0)
+sum = sum + k;
+j = k - j;
+++i;
+}
+printf("%ld\n", sum);
+return (0);
 }
