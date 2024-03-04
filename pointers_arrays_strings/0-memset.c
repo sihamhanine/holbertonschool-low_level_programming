8bf110ae90996aa5801342fb87ca6355f5646813
sihamelani <siham.elani17@gmail.com>
2024-03-04 11:26:38 +0000

    Task 0. Write a function that fills memory with a constant byte.

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * char *_memset  that fills memory with a constant byte
- *
+ * char *_memset  that fills memory
+ * with a constant byte
  *
  * @s: le caractere a verifier
  * @b: la valeur a ajouter
  * @n: le nombre d'octet
  *
- * Return: le pointeur sur la zone m moire s
+ * Return: s le pointeur
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
