69eb2ffd53139d71c832737da1efd5b26864eb61
sihamelani <siham.elani17@gmail.com>
2024-03-04 18:03:35 +0000

    Task 5. Write a function that locates a substring.

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strstr - Function that locates a substring.
+ *
+ * @haystack: la chaine a verifier
+ * @needle: les caracteres a verifier
+ *
+ * Return: pointer visant sa premiere occurence
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int i, j;
+for (i = 0; haystack[i] != '\0' ; i++)
+{
+for (j = 0 ; needle[j] != '\0' ; j++)
+{
+if (haystack[i] == needle[j])
+{
+return (needle);
+break;
+}
+}
+}
+return (haystack);
+}
