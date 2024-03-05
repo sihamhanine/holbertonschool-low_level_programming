cc0c42bf28647fbb4cc48018da4a57886b82326d
sihamelani <siham.elani17@gmail.com>
2024-03-05 21:23:40 +0000

    Task 0. Write a function that prints a string, followed by a new line.

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -10,10 +10,14 @@
  */
 void _puts_recursion(char *s)
 {
-  int i;
-  for (i = 0 ; s[i] != '\0'; i++)
+  
+  if (*s == '\0')
     {
-      _putchar(s[i]);
+      _putchar('\n');
+      return;
     }
-  _putchar('\n');
+  _putchar(*s);
+  s++;
+  _puts_recursion(s);
+  
 }
