63d72c145bdacf50df3a2903e49742d669e360cb
sihamelani <siham.elani17@gmail.com>
2024-03-06 10:53:05 +0000

    Task 4.Write a function that searches a string for any of a set of bytes.

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -12,17 +12,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-int i, j;
-for (i = 0 ; s[i] >= '\0' ; i++)
+int j;
+while (*s)
 {
-for (j = 0 ; accept[j] >= '\0' ; j++)
+for (j = 0 ; accept[j] ; j++)
 {
-if (accept[j] == s[i])
-{
-return (s + i);
+if (*s == accept[j])
+return (s);
 }
+s++;
 }
-}
-return (NULL);
+return ('\0');
 }
 
