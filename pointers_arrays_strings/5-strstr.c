cd4fc7345d0374827377880809ee67a113efb404
sihamelani <siham.elani17@gmail.com>
2024-03-06 13:20:38 +0000

    Task 5.Write a function that locates a substring.

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,10 +11,10 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-int i, j;
+  int i, j;
 for (i = 0; haystack[i] != '\0' ; i++)
 {
-for (j = 0 ; needle[j] != '\0' ; j++)
+for (j = 0 ; needle[j] != '\0' ; i++)
 {
 while (haystack[i] == needle[j])
 {
@@ -22,7 +22,7 @@ haystack++;
 needle++;
 }
 }
-if (needle[i] == '\0')
+if (needle[j] == '\0')
 return (haystack);
 }
 return (0);
