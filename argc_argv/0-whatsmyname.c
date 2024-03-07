815fcd13ab8f3d5320c48e9bf9fb9b032736e035
sihamelani <siham.elani17@gmail.com>
2024-03-07 19:54:13 +0000

    Task 0 Write a program that prints its name, followed by a new line.

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main -  program that prints its name,
+ * main - program that prints its name,
  * followed by a new line.
  *
  * @argc: le nombre arguments
