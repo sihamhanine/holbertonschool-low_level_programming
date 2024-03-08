f51787ead493fa7f0ae9f7524b6d63f730700f1f
sihamelani <siham.elani17@gmail.com>
2024-03-08 17:49:49 +0000

    Task 3 Write a program that multiplies two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,14 +10,16 @@
  *
  * Return: NOT NULL.
  */
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 int n1 = 0, n2 = 0;
+int mult;
 if (argc == 3)
 {
 n1 = atoi(argv[1]);
 n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
+mult = n1 * n2;
+printf("%d\n", mult);
 }
 else
 {
