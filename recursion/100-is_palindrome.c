d2832ac97459dee178741e75cf9751f5731d183b
sihamelani <siham.elani17@gmail.com>
2024-03-08 21:58:46 +0000

    Task 7.Write a function that returns 1 if a string is a palindrome and 0 if not

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,42 +1,58 @@
 #include "main.h"
-int _check_palindrome(char *s, int left, int right);
+int comparer(char *s, int n1, int n2);
+int _strlen_recursion(char *s);
+
+
+/**
+ * _strlen_recursion - return the lenght of a string
+ *
+ *
+ * @s: la chaine a verifier
+ * Return: la longueur de la chaine
+ */
+int _strlen_recursion(char *s)
+{
+if(*s == '\0')
+{
+  return (0);
+ }
+  else
+  return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * comparer - Function that comparer chaque caractere
+ * de la chaine
+ * @s: la chaine a verifier
+ * @n1: l'indice d'iteration droite
+ * @n2: l'indice d'iteration gauche
+ *
+ * Return: 0
+ */
+int comparer(char *s, int n1, int n2)
+{
+  if (*(s + n1) == *(s + n2))
+    {
+    if (n1 == n2 || n1 == n2 + 1)
+      return (1);
+  return (0 + comparer(s, n1 + 1, n2 - 1));
+ }
+
+return (0);
+
+}
+
 
 /**
  * is_palindrome - Function  that returns 1 if
  * a string is a palindrome and 0 if not.
- *
- *
  * @s: la chaine a verifier
- * Return: 1 si la chaine est palindrome sinon return 0
+ * Return: 1 si la chaine est palindrome sinon 0
  */
 int is_palindrome(char *s)
 {
-while(*s != '\0')
+if(*s == '\0')
 {
-return _check_palindrome(s, 0, *s - 1);
+  return (1);
+ }
+ return (comparer(s, 0, _strlen_recursion(s) - 1));
 }
-s++
-}
-/**
- * _check_palindrome - Function that returns 1
- * if a string is a palindrome and 0 if not.
- *
- * @s: la chaine a verifier
- * @left: l'indice d'iteration droite
- * @right: l'indice d'iteration gauche
- *
- * Return: 1 si la chaine est palindrome sinon 0
- */
-int _check_palindrome(char *s, int left, int right)
-{
-if (left >= right)
-{
-return (1);
-}
-else
-if (s[left] == s[right])
-{
-return _check_palindrome(s, left + 1, right - 1);
-}
-}
-
