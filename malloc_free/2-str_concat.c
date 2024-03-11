9db3aeee30bc9138ebcb3ab7bbed77fa9a62d0ad
sihamelani <siham.elani17@gmail.com>
2024-03-11 17:14:24 +0000

    Task 2. Write a function that concatenates two strings.

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,27 +14,17 @@ char *str_concat(char *s1, char *s2)
 int i, j, k, size;
 char *p;
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
-}
 while (s1[i] != '\0')
-{
 i++;
-}
 while (s2[j] != '\0')
-{
 j++;
-}
 size = i + j;
 p = malloc((sizeof(char) * size) +1);
 if (p == NULL)
-{
 return (NULL);
-}
 else
 j = 0;
 for (k = 0 ; k < size ; k++)
