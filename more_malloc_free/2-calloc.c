5b84a03fced3ee16d8f444ecf92fb5c607fd4381
sihamelani <siham.elani17@gmail.com>
2024-03-13 14:07:22 +0000

    Task 2. Write a function that allocates memory for an array, using malloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,22 +12,21 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  int *p;
-  unsigned int i;
-  if (nmemb == 0 || size == 0)
-    {
-      return (NULL);
-    }
-  p = malloc(sizeof(int) * nmemb);
-  if (p == NULL)
-    {
-      free(p);
-      return (NULL);
-    }
-  else
-    for (i = 0 ; i < size ; i++)
-      {
-	p[i] = i + 1;
-      }
-  return (p);
+int *p;
+unsigned int i = 0;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+p = (int *) malloc(sizeof(int) * nmemb);
+if (p == NULL)
+{
+return (NULL);
+}
+else
+for (i = 0 ; i < size ; i++)
+{
+p[i] = 0;
+}
+return (p);
 }
