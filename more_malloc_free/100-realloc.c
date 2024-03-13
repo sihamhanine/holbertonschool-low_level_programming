75435b1ba11ee2ec68d6217b494d8ee34d77d634
sihamelani <siham.elani17@gmail.com>
2024-03-13 16:29:08 +0000

    Task 4 . Write a function that reallocates a memory block using malloc and free

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_realloc - Function that reallocates a memory
+ * block using malloc and free
+ *
+ * @ptr: pointeur vers la memoire deja alloue
+ * @old-size: la taille de memoire deja alloue
+ * @new_size: la taille de la nouvelle memoire a allouee
+ *
+ * Return: pointer alloue else return NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *p;
+unsigned int i;
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+{
+p = malloc(new_size);
+if (p == NULL)
+return (NULL);
+return (p);
+}
+if (new_size > old_size)
+{
+p = malloc(new_size);
+if (p == NULL)
+return (NULL);
+for (i = 0 ; i < old_size && i < new_size ; i++)
+p[i] = ((char *) ptr)[i];
+free(ptr);
+}
+return (p);
+}
