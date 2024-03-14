4b2a0297c587d48f48af6c27a0d38f03bc51a55d
sihamelani <siham.elani17@gmail.com>
2024-03-14 19:10:24 +0000

    Task 4. Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,14 +12,44 @@
  * Return: struct dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
-{
+{ int i, j, lent1 = 0, lent2 = 0;
+  
   dog_t *dog;
+  if (name == NULL || age < 0 || owner == NULL)
+    return (NULL);
+  
   dog = malloc(sizeof(dog_t));
   if (dog == NULL)
     return (NULL);
-  dog->name = name;
+
+  while (name[lent1] != '\0')
+    lent1++;
+  while (owner[lent2] != '\0')
+    lent2++;
+  dog->name = malloc(sizeof(char) * (lent1 + 1));
+  if (dog->name == NULL)
+    {
+      free(dog);
+  return (NULL);
+    }
+  dog->owner = malloc(sizeof(char) * (lent2 + 1));
+  if (dog->owner == NULL)
+    {
+      free(dog->name);
+      free(dog);
+      return (NULL);
+    }
+  
+  for (i = 0 ; name[i] != '\0' ; i++)
+    {
+    dog->name[i] = name[i];
+  dog->name[i] = '\0';
+    }
+  for (j = 0 ; owner[j] != '\0' ; j++)
+    {
+      dog->owner[i] = owner[j];
+      dog->owner[i] = '\0';
+    }
   dog->age = age;
-  dog->owner = owner;
   return (dog);
 }
-
