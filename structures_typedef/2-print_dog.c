1107858bca21d758646d5ae02b83bb98c4e1695d
sihamelani <siham.elani17@gmail.com>
2024-03-14 13:52:32 +0000

    Task 2.Write a function that prints a struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,11 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
- * print_dog - Function that prints a struct dog
+ * print_dog - Function that prints
+ * a struct dog
  * 
  *
- * @d: le pointeur qui pointe vers la structure dog
+ * @d: le pointeur qui pointe vers
+ * la structure dog
  *
  * imprimer la structure dog
  */
@@ -13,17 +16,17 @@ void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-  if (d->name != NULL)
-      printf("Name: %s\n", d->name);
-  else
-    printf("Name: (nil)\n");
-  if (d->age != 0)
-    printf("Age: %f\n", d->age);
-  else
-    printf("Age: 0.000000\n");
-  if (d->owner !=NULL)
-    printf("Owner: %s\n", d->owner);
-  else
-    printf("Owner: (nil)\n");
- }
+if (d->name != NULL)
+printf("Name: %s\n", d->name);
+else
+printf("Name: (nil)\n");
+if (d->age != 0)
+printf("Age: %f\n", d->age);
+else
+printf("Age: 0.000000\n");
+if (d->owner != NULL)
+printf("Owner: %s\n", d->owner);
+else
+printf("Owner: (nil)\n");
+}
 }
