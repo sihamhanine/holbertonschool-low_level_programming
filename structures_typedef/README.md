e68ef3c38cf34aacba94041ce94c3cbf39a22c38
sihamelani <siham.elani17@gmail.com>
2024-03-14 10:48:50 +0000

    Task 0 Define a new type struct dog with the following elements:

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+My first commit structures type-def
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - with the following elements:
+ *
+ * @name: le premier membre de type char
+ * @age: le deuxieme membre de type float
+ * @owner: le troixieme membre de type char
+ * decrire une structure sous nom de dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
