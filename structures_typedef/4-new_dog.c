bf493fd340ebad02268249aaa629cabb2f16acb7
sihamelani <siham.elani17@gmail.com>
2024-03-14 20:26:52 +0000

    Task 4. Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,46 +12,41 @@
  * Return: struct dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
-{ int i, j, lent1 = 0, lent2 = 0;
-  
-  dog_t *dog;
-  if (name == NULL || age < 0 || owner == NULL)
-    return (NULL);
-  
-  dog = malloc(sizeof(dog_t));
-  if (dog == NULL)
-    return (NULL);
-
-  while (name[lent1] != '\0')
-    lent1++;
-  while (owner[lent2] != '\0')
-    lent2++;
-  dog->name = malloc(sizeof(char) * (lent1 + 1));
-  if (dog->name == NULL)
-    {
-      free(dog);
-  return (NULL);
-    }
-  dog->owner = malloc(sizeof(char) * (lent2 + 1));
-  if (dog->owner == NULL)
-    {
-      free(dog->name);
-      free(dog);
-      return (NULL);
-    }
-  
-  for (i = 0 ; name[i] != '\0' ; i++)
-    {
-    dog->name[i] = name[i];
-    }
-  dog->name[i] = '\0';
-    
-  for (j = 0 ; owner[j] != '\0' ; j++)
-    {
-      dog->owner[j] = owner[j];
-    }
-      dog->owner[j] = '\0';
-    
-  dog->age = age;
-  return (dog);
+{
+int i, j, lent1 = 0, lent2 = 0;
+dog_t *dog;
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+while (name[lent1] != '\0')
+lent1++;
+while (owner[lent2] != '\0')
+lent2++;
+dog->name = malloc(sizeof(char) * (lent1 + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+dog->owner = malloc(sizeof(char) * (lent2 + 1));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+for (i = 0 ; name[i] != '\0' ; i++)
+{
+dog->name[i] = name[i];
+}
+dog->name[i] = '\0';
+for (j = 0 ; owner[j] != '\0' ; j++)
+{
+dog->owner[j] = owner[j];
+}
+dog->owner[j] = '\0';
+dog->age = age;
+return (dog);
 }
