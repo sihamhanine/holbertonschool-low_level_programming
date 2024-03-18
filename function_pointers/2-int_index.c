30c68569cb27086246c03fa634e5e928158de598
sihamelani <siham.elani17@gmail.com>
2024-03-18 14:36:23 +0000

    Task 2. Write a function that searches for an integer.

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - Function Write that searches for an integer.
+ *
+ * @array: le tableau d'elements
+ * @size: la taille en octet du tableau
+ * @cmp: le pointeur de la fonction
+ *
+ * Return: index of the first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+  int i;
+  if (size > 0)
+    {
+      
+  if (array != NULL && cmp != NULL)
+    {
+      for ( i = 0 ; i < size ; i++)
+	{
+	  if (cmp(array[i]))
+	    return (i);
+	}
+    }
+    
+    }
+  return (-1);
+}
