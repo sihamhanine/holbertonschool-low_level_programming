0b6b43d6da0651bc7f94e9526b6a203337b8e038
sihamelani <siham.elani17@gmail.com>
2024-03-18 14:44:25 +0000

    Task 2. Write a function that searches for an integer.

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -10,19 +10,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-  int i;
-  if (size > 0)
-    {
-      
-  if (array != NULL && cmp != NULL)
-    {
-      for ( i = 0 ; i < size ; i++)
-	{
-	  if (cmp(array[i]))
-	    return (i);
-	}
-    }
-    
-    }
-  return (-1);
+int i;
+if (size > 0)
+{
+if (array != NULL && cmp != NULL)
+{
+for ( i = 0 ; i < size ; i++)
+{
+if (cmp(array[i]))
+return (i);
+}
+}
+}
+return (-1);
 }
