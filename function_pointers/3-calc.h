5f2d3b9d0369abce732d5db7406f7e60f8488257
sihamelani <siham.elani17@gmail.com>
2024-03-19 13:05:38 +0000

    Task 3

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,6 +1,6 @@
 #ifndef calc_h
 #define calc_h
-#include <stddef.h> 
+#include <stddef.h>
 /**
  * struct op - Struct op
  *
@@ -9,8 +9,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+char *op;
+int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
