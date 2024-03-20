a23522aad40df51d516617cdb00184523ba9b2a6
sihamelani <siham.elani17@gmail.com>
2024-03-20 13:11:01 +0000

    Task 2.Write a function that prints strings, followed by a new line.

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * print_strings - Function that prints strings,
+ * followed by a new line.
+ *
+ * @separator: le separateur entre les chaines
+ * @n: le nombre des arguments
+ *
+ * Return: no thing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+  unsigned int i;
+  va_list argc;
+  va_start(argc, n);
+  if (separator == NULL)
+    separator = "";
+  for (i = 0 ; i < n ; i++)
+    {
+    printf("%s", va_arg(argc, char *));
+  if (i < n - 1)
+    printf("%s", separator);
+    }
+  printf("\n");
+  va_end(argc);
+}
