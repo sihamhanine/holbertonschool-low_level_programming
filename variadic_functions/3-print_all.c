c2052cf8196f6191243daf8415a03b370cc970a8
sihamelani <siham.elani17@gmail.com>
2024-03-21 20:11:22 +0000

    Task 3. Write a function that prints anything.

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,42 +12,42 @@
  */
 void print_all(const char * const format, ...)
 {
-  int i, etat;
-  char *str;
-  va_list argc;
-  va_start(argc, format);
-  i = 0;
-  while (format != NULL && format[i] != '\0')
-    {
-      switch (format[i])
-	{
-      case 'c':
-	printf("%c", va_arg(argc, int));
-	etat = 0;
-	break;
-	case 'i':
-	  printf("%d", va_arg(argc, int));
-	  etat = 0;
-	  break;
-	case 'f':
-	  printf("%f", va_arg(argc, double));
-	  etat = 0;
-	  break;
-	case 's':
-	  str = va_arg(argc, char *);
-	  if (str == NULL)
-	    str = "(nil)";
-	  printf("%s", str);
-	  etat = 0;
-	  break;
-	default:
-	  etat = 1;
-	  break;
-	}
-      if (format[i + 1] != '\0' && etat == 0)
-	printf(", ");
-      i++;
-    }
-  printf("\n");
-  va_end(argc);
+int i, etat;
+char *str;
+va_list argc;
+va_start(argc, format);
+i = 0;
+while (format != NULL && format[i] != '\0')
+{
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(argc, int));
+etat = 0;
+break;
+case 'i':
+printf("%d", va_arg(argc, int));
+etat = 0;
+break;
+case 'f':
+printf("%f", va_arg(argc, double));
+etat = 0;
+break;
+case 's':
+str = va_arg(argc, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s", str);
+etat = 0;
+break;
+default:
+etat = 1;
+break;
+}
+if (format[i + 1] != '\0' && etat == 0)
+printf(", ");
+i++;
+}
+printf("\n");
+va_end(argc);
 }
