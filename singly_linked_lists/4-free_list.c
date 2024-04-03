a6a240ac4b05557f8f1e1e8350ba5610c58d82a3
sihamelani <siham.elani17@gmail.com>
2024-04-03 08:32:41 +0000

    Task 4 betty modification

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -5,20 +5,19 @@
 #include <string.h>
 /**
  * free_list - Function that frees a list_t list.
- * 
+ *
  * @head: the pointer of the first element
  *
  * Return: no thing
  */
 void free_list(list_t *head)
 {
-  list_t *temp;
-  while (head != NULL)
-    {
-      temp = head;
-      head = head->next;
-      free(temp->str);
-      free(temp);
-      
-    }
+list_t *temp;
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp->str);
+free(temp);
+}
 }
