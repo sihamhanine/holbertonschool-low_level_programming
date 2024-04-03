7761ce8dd7ddfd98feb4fe550c288c6fb3da15d7
sihamelani <siham.elani17@gmail.com>
2024-04-03 14:05:07 +0000

    Task8 supprimer un element a une indice donne et betty style

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -14,31 +14,31 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 dlistint_t *temp;
 unsigned int i = 0;
- if (*head == NULL)
-   return (-1);
- temp = *head;
+if (*head == NULL)
+return (-1);
+temp = *head;
 while (temp != NULL && i != index)
 {
 temp = temp->next;
 i++;
 }
- if (temp == NULL)
-   {
-   return (-1);
-   }
- 
-    if (temp->prev != NULL)
-      {
-	temp->prev->next = temp->next;
-      }
-    else
-      {
-      *head = temp->next;
-      }
-    if (temp->next != NULL)
-      {
-      temp->next->prev = temp->prev;
-      }
-    free(temp);
-    return (1);
+if (temp == NULL)
+{
+return (-1);
 }
+if (temp->prev != NULL)
+{
+temp->prev->next = temp->next;
+}
+else
+{
+*head = temp->next;
+}
+if (temp->next != NULL)
+{
+temp->next->prev = temp->prev;
+}
+free(temp);
+return (1);
+}
+
