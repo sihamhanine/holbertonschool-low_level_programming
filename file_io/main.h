65b0814f5d9ad82cf4059a50e452433845c73bd4
sihamelani <siham.elani17@gmail.com>
2024-04-05 11:26:26 +0000

    Task 1 Create a function that creates a file.

--- file_io/main.h
+++ file_io/main.h
@@ -5,5 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 int _putchar(char c);
+int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
