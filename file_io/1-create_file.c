d4acea4eb54ef5de267036173c94450c5ea7e995
sihamelani <siham.elani17@gmail.com>
2024-04-05 11:34:58 +0000

    modification task1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ if (filename == NULL)
 {
 return (-1);
 }
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 return (-1);
 if (text_content != NULL)
