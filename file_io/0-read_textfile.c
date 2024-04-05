2144d9632e1642eb561d1939c431aeb5f02c7024
sihamelani <siham.elani17@gmail.com>
2024-04-05 09:25:21 +0000

    Task0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * read_textfile - Function that reads a text file
+ * and prints it to the POSIX standard output.
+ * @filename: the name of file to read
+ * @letters: the number of letters to read
+ *
+ * Return: number of byte to read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  int fd;
+  ssize_t letters_readed;
+  char *text = malloc(sizeof(char *) * letters);
+  if ((filename == NULL) || (letters == NULL)
+    {
+      return (0);
+    }
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+      return (0);
+    letters_readed = read(fd, text, letters);
+    write(STDOUT_FILENO, text, letters_readed);
+    close(fd);
+    return (letters_readed);
+  
+}
+    
