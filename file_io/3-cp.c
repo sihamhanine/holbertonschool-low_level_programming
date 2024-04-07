0f2907757a5bf36030a3a98529746b360d66c2c4
sihamelani <siham.elani17@gmail.com>
2024-04-07 08:56:31 +0000

    Task3. cp file_from file_to modification

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -42,19 +42,22 @@ error_and_exit(97, "Usage: cp file_from file_to");
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
-error_and_exit(98, "Error: Can't read from file");
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
 }
 file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (file_to == -1)
 {
-error_and_exit(99, "Error: Can't write to file");
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
 }
 while ((bytes_readed = read(file_from, buf, 1024)) > 0)
 {
 bytes_written = write(file_to, buf, bytes_readed);
 if (bytes_written == -1 || bytes_written != bytes_readed)
 {
-error_and_exit(99, "Error: Can't write to file");
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
 }
 }
 if (bytes_readed == -1)
