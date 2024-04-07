0a3d9d90b5f293dffc5bc838e74a108e21a559fa
sihamelani <siham.elani17@gmail.com>
2024-04-07 08:30:47 +0000

    Task3. cp file_from file_to

---
+++ file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+/**
+ * error_and_exit - Function that verify
+ * condition and get error and exit number
+ * @code: the code to exit
+ * @meassge: error message
+ *
+ * Return: no thing
+ */
+void error_and_exit(int code, const char *message)
+{
+dprintf(STDERR_FILENO, "%s\n", message);
+exit(code);
+}
+/**
+ * main - copy file_from to file_to
+ *
+ * @argc: number of argument
+ * @argv: la chaine d'argument
+ *
+ * Return : always 0;
+ */
+int main(int argc, char **argv)
+{
+int file_from, file_to;
+char buf[1024];
+ssize_t bytes_readed;
+ssize_t bytes_written;
+if (argc != 3)
+{
+error_and_exit(97, "Usage: cp file_from file_to");
+}
+file_from = open(argv[1], O_RDONLY);
+if (file_from == -1)
+{
+error_and_exit(98, "Error: Can't read from file");
+}
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (file_to == -1)
+{
+error_and_exit(99, "Error: Can't write to file");
+}
+while ((bytes_readed = read(file_from, buf, 1024)) > 0)
+{
+bytes_written = write(file_to, buf, bytes_readed);
+if (bytes_written == -1 || bytes_written != bytes_readed)
+{
+error_and_exit(99, "Error: Can't write to file");
+}
+}
+if (bytes_readed == -1)
+{
+error_and_exit(98, "Error while reading from file");
+}
+if (close(file_from) == -1 || close(file_to) == -1)
+{
+error_and_exit(100, "Error: Can't close file descriptor");
+}
+return 0;
+} 
--- file_io/main.h
+++ file_io/main.h
@@ -9,4 +9,5 @@ int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_and_exit(int code, const char *message);
 #endif
